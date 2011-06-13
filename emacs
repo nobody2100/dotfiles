e6928cffe6b3bb40842c95ceefc9a06aa39cf16e
AYH <nobody2100@googlemail.com>
2011-06-13 18:04:13 +0200

    before github

--- emacs
+++ emacs
@@ -2,3 +2,37 @@
 (setq make-backup-files         nil) ; Don't want any backup files 
 (setq auto-save-list-file-name  nil) ; Don't want any .saves files 
 (setq auto-save-default         nil) ; Don't want any auto saving 
+
+;#
+;# programming
+;#
+
+; C-F11 to compile
+(global-set-key [(control f11)] 'compile)
+
+
+; turn on random customization options
+(custom-set-variables
+  '(column-number-mode t)
+  '(completion-ignore-case t)
+  '(read-file-name-completion-ignore-case t)
+  '(default-major-mode 'text-mode)  ; default to text mode, not fundamental
+  '(diff-default-read-only nil)  ; don't make diff mode (for patches) read-only
+  '(diff-switches "-u --ignore-all-space")
+
+  '(line-number-mode t)
+
+  '(require-final-newline t)
+  '(sentence-end-double-space nil)
+ 
+ 
+  '(history-length t)      ; no max length for minibuffer history; store it all
+  '(history-delete-duplicates t)
+  '(isearch-allow-scroll t)
+  '(isearch-case-fold-search t)
+  '(set-mark-command-repeat-pop t) ; allow repeating C-space after C-u
+
+)
+
+
+
--- install.sh
+++ install.sh
@@ -1,21 +1,21 @@
-echo "This install script will modify the following configuration files in your home directory:\n\t.bashrc\n\t.bash_profile\n\t.emacs\n\t.screenrc"
+echo -e "This install script will modify the following configuration files in your home directory:\n\t.bashrc\n\t.bash_profile\n\t.emacs\n\t.screenrc"
 
 # bashrc
-mv ~/.bashrc ~/.bashrc_bak
+#mv ~/.bashrc ~/.bashrc_bak
 echo ". ~/bin/dotfiles/bashrc" > ~/.bashrc
 
 # bash_profile: the same as bashrc
-mv ~/.bash_profile ~/.bash_profile_bak
-echo -e "if [ -f ~/.bashrc ];\nthen\n\tsource ~/.bashrc\nfi" > ~/.bashrc
+#mv ~/.bash_profile ~/.bash_profile_bak
+echo -e "if [ -f ~/.bashrc ];\nthen\n\tsource ~/.bashrc\nfi" > ~/.bash_profile
 
 # emacs
-mv ~/.emacs ~/.emacs_bak
-ln -s ~/bin/dotfiles/emacs ~/.emacs
+#mv ~/.emacs ~/.emacs_bak
+ln -sfv ~/bin/dotfiles/emacs ~/.emacs
 
 # ssh config
-mv ~/.emacs ~/.emacs_bak
-ln -s ~/bin/dotfiles/ssh/config ~/.ssh/config
+#mv ~/.emacs ~/.emacs_bak
+ln -sfv ~/bin/dotfiles/ssh/config ~/.ssh/config
 
 # screenrc
-mv ~/.screenrc ~/.screenrc_bak
-ln -s ~/bin/dotfiles/screenrc ~/.screenrc
+#mv ~/.screenrc ~/.screenrc_bak
+ln -sfv ~/bin/dotfiles/screenrc ~/.screenrc
