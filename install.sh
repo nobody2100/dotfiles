22cfe40f77600503c07c26d85bd0ffcb700a1b85
AYH <nobody2100@googlemail.com>
2011-06-13 18:34:44 +0200

    can place dotfiles anywhere i want

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
+#!/bin/bash
+
 echo -e "This install script will modify the following configuration files in your home directory:\n\t.bashrc\n\t.bash_profile\n\t.emacs\n\t.screenrc"
+
+path=$(pwd)
+
+echo "the path to the dotfiles is $path"
 
 # bashrc
 #mv ~/.bashrc ~/.bashrc_bak
-echo ". ~/bin/dotfiles/bashrc" > ~/.bashrc
+echo ". $path/bashrc" > ~/.bashrc
 
 # bash_profile: the same as bashrc
 #mv ~/.bash_profile ~/.bash_profile_bak
@@ -10,12 +16,12 @@ echo -e "if [ -f ~/.bashrc ];\nthen\n\tsource ~/.bashrc\nfi" > ~/.bash_profile
 
 # emacs
 #mv ~/.emacs ~/.emacs_bak
-ln -sfv ~/bin/dotfiles/emacs ~/.emacs
+ln -sfv $path/emacs ~/.emacs
 
 # ssh config
 #mv ~/.emacs ~/.emacs_bak
-ln -sfv ~/bin/dotfiles/ssh/config ~/.ssh/config
+ln -sfv $path/ssh/config ~/.ssh/config
 
 # screenrc
 #mv ~/.screenrc ~/.screenrc_bak
-ln -sfv ~/bin/dotfiles/screenrc ~/.screenrc
+ln -sfv $path/screenrc ~/.screenrc
